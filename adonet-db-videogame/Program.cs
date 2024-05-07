aa5f6b2c72e882e26f5e767be87fc9471a8596b2
enricoforesta <enrico.foresta97@gmail.com>
2024-05-07 16:49:21 +0200

    feat: fine esercizio

--- adonet-db-videogame/Program.cs
+++ adonet-db-videogame/Program.cs
@@ -4,7 +4,52 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            bool continua = true;
+            Videogame NuovoVideogioco = new Videogame("Call of Duty", "owerefihiadfh", "20/11/2024" , DateTime.Now, DateTime.Now, 3);
+            while (continua)
+            {
+                Console.WriteLine("Menu:");
+                Console.WriteLine("1. Inserire un nuovo videogioco");
+                Console.WriteLine("2. Ricercare un videogioco per ID");
+                Console.WriteLine("3. Ricercare tutti i videogiochi aventi il nome contenente una determinata stringa");
+                Console.WriteLine("4. Cancellare un videogioco");
+                Console.WriteLine("5. Chiudere il programma");
+
+                Console.Write("Scelta: ");
+                string scelta = Console.ReadLine();
+
+                switch (scelta)
+                {
+                    case "1":
+                        VideogameManager.InserisciVideogioco(NuovoVideogioco);
+                        break;
+                    case "2":
+                        Console.WriteLine("inserire ID");
+                        int idInput = Convert.ToInt32(Console.ReadLine());
+                        VideogameManager.RicercaVideogiocoId(idInput);
+                        break;
+                    case "3":
+                        Console.WriteLine("inserire Nome");
+                        string nomeInput = Console.ReadLine();
+                        VideogameManager.RicercaVideogiocoNome(nomeInput);
+                        break;
+                    case "4":
+                        Console.WriteLine("inserire ID da eliminare");
+                        int idInputDelete = Convert.ToInt32(Console.ReadLine());
+                        VideogameManager.CancellareVideogiocoId(idInputDelete);
+                        break;
+                    case "5":
+                        continua = false;
+                        break;
+                    default:
+                        Console.WriteLine("Scelta non valida.");
+                        break;
+                }
+
+                Console.WriteLine();
+            }
         }
+
     }
 }
+
--- adonet-db-videogame/Videogame.cs
+++ adonet-db-videogame/Videogame.cs
@@ -9,32 +9,32 @@ using System.Threading.Tasks;
 
 namespace adonet_db_videogame
 {
-    public class Videogame
+    public class Videogame 
     {
 
         public int Id {  get; set; }
         public string Name { get; set; }
         public string Overview { get; set; }
-        public string Release_date { get; set; }
+        public DateOnly Release_date { get; set; }
         public DateTime Created_at { get; set; }
         public DateTime Update_at { get; set; }
 
         public int Software_house_id { get; set; }
 
-        public Videogame(string name, string overview,string release_date,DateTime created_at, DateTime update_at, int software_house_id) 
+        public Videogame(string name, string overview, string release_date,DateTime created_at, DateTime update_at, int software_house_id) 
         {
             this.Name = name;
             this.Overview = overview;
-            this.Release_date = release_date;
+            this.Release_date = DateOnly.ParseExact(release_date, "dd/MM/yyyy") ;
             this.Created_at = created_at;
             this.Update_at = update_at;
             this.Software_house_id = software_house_id;
         }
     }
 
-    public static class VideogameManager
+    public static class VideogameManager 
     {
-        public static string SqlStringa = "Data Source=localhost; Initial Catalog=db_videogames; Integrated Security=True; Encrypt=True; Trust Server Certificate=True";
+        public static string SqlStringa = "Data Source=localhost; Initial Catalog=db_videogames; Integrated Security=True;";
 
         public static void InserisciVideogioco(Videogame videogame)
         {
@@ -75,15 +75,21 @@ namespace adonet_db_videogame
                 using SqlDataReader reader = cmd.ExecuteReader();
                 if (reader.Read())
                 {
-                    int id = reader.GetInt32(reader.GetOrdinal("Id"));
-                    string name = reader.GetString(reader.GetOrdinal("name"));
-                    string overview = reader.GetString(reader.GetOrdinal("overview"));
-                    string release_date = reader.GetString(reader.GetOrdinal("release_date"));
-                    DateTime created_at = reader.GetDateTime(reader.GetOrdinal("created_at"));
-                    DateTime updated_at = reader.GetDateTime(reader.GetOrdinal("updated_at"));
-                    int software_house_id = reader.GetInt32(reader.GetOrdinal("software_house_id"));
+                    int id = reader.GetOrdinal("Id");
+                    int name = reader.GetOrdinal("name");
+                    int overview = reader.GetOrdinal("overview");
+                    int release_date = reader.GetOrdinal("release_date");
+                    int created_at = reader.GetOrdinal("created_at");
+                    int updated_at = reader.GetOrdinal("updated_at");
+                    int software_house_id = reader.GetOrdinal("software_house_id");
 
-                    Console.WriteLine($"{id}-{name}-{overview}-{release_date}-{created_at}-{updated_at}-{software_house_id}");
+                    Console.WriteLine($"ID: {reader.GetInt64(id)}");
+                    Console.WriteLine($"Nome: {reader.GetString(name)}");
+                    Console.WriteLine($"Descrizione: {reader.GetString(overview)}");
+                    Console.WriteLine($"Data di rilascio: {reader.GetDateTime(release_date)}");
+                    Console.WriteLine($"Data di Creazione: {reader.GetDateTime(created_at)}");
+                    Console.WriteLine($"Data ultima modifica: {reader.GetDateTime(updated_at)}");
+                    Console.WriteLine($"Software house Id: {reader.GetInt64(software_house_id)}");
                 }
                 else
                 {
@@ -101,9 +107,8 @@ namespace adonet_db_videogame
             using SqlConnection connessioneSql = new SqlConnection(SqlStringa);
             try
             {
-                nome = Console.ReadLine();
                 connessioneSql.Open();
-                string query = @"SELECT * FROM videogames WHERE name = @nome";
+                string query = @"SELECT * FROM videogames WHERE name LIKE '%' + @nome + '%'";
 
                 using SqlCommand cmd = new SqlCommand(query, connessioneSql);
                 cmd.Parameters.Add(new SqlParameter("@nome", nome));
@@ -111,21 +116,25 @@ namespace adonet_db_videogame
                 using SqlDataReader reader = cmd.ExecuteReader();
                 if (reader.Read())
                 {
-                    int id = reader.GetInt32(reader.GetOrdinal("Id"));
-                    string name = reader.GetString(reader.GetOrdinal("name"));
-                    string overview = reader.GetString(reader.GetOrdinal("overview"));
-                    string release_date = reader.GetString(reader.GetOrdinal("release_date"));
-                    DateTime created_at = reader.GetDateTime(reader.GetOrdinal("created_at"));
-                    DateTime updated_at = reader.GetDateTime(reader.GetOrdinal("updated_at"));
-                    int software_house_id = reader.GetInt32(reader.GetOrdinal("software_house_id"));
+                    int id = reader.GetOrdinal("Id");
+                    int name = reader.GetOrdinal("name");
+                    int overview = reader.GetOrdinal("overview");
+                    int release_date = reader.GetOrdinal("release_date");
+                    int created_at = reader.GetOrdinal("created_at");
+                    int updated_at = reader.GetOrdinal("updated_at");
+                    int software_house_id = reader.GetOrdinal("software_house_id");
 
-                    Console.WriteLine($"{id}-{name}-{overview}-{release_date}-{created_at}-{updated_at}-{software_house_id}");
-
+                    Console.WriteLine($"ID: {reader.GetInt64(id)}");
+                    Console.WriteLine($"Nome: {reader.GetString(name)}");
+                    Console.WriteLine($"Descrizione: {reader.GetString(overview)}");
+                    Console.WriteLine($"Data di rilascio: {reader.GetDateTime(release_date)}");
+                    Console.WriteLine($"Data di Creazione: {reader.GetDateTime(created_at)}");
+                    Console.WriteLine($"Data ultima modifica: {reader.GetDateTime(updated_at)}");
+                    Console.WriteLine($"Software house Id: {reader.GetInt64(software_house_id)}");
                 }
                 else
                 {
-                    Console.WriteLine("Non è stato trovato alcun videogioco con l'ID specificato");
-
+                    Console.WriteLine("Non è stato trovato alcun videogioco con nome specificato");
                 }
             }
             catch (Exception ex)
@@ -133,9 +142,27 @@ namespace adonet_db_videogame
                 Console.WriteLine(ex.Message);
             }
         }
-        public static void CancellareVideogiocoId(Videogame videogame)
+        public static void CancellareVideogiocoId(int videogameId)
         {
+            using (SqlConnection connessioneSql = new SqlConnection(SqlStringa))
+            {
+                try
+                {
+                    connessioneSql.Open();
+                    string query = @"DELETE FROM videogames WHERE Id = @videogameId";
 
+                    using (SqlCommand cmd = new SqlCommand(query, connessioneSql))
+                    {
+                        cmd.Parameters.AddWithValue("@videogameId", videogameId);
+                        cmd.ExecuteNonQuery();
+                        Console.WriteLine("Videogioco eliminato con successo.");
+                    }
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine(ex.Message);
+                }
+            }
         }
     }
 }
